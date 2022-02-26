7523d120c2ddf4bd1ea3676f5ba36f2a32775cd9
mizan <dewan.mizanur911@gmail.com>
2022-02-26 15:09:53 +0600

    navbar design completed

--- src/Pages/Shared/BootstrapNavbar/BootstrapNavbar.js
+++ src/Pages/Shared/BootstrapNavbar/BootstrapNavbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import './BootstrapNavbar.css';
 import { Link } from 'react-router-dom';
 
 const BootstrapNavbar = () => {
+    // const user = <FontAwesomeIcon icon={faUser} />
+    // const user = <FontAwesomeIcon icon="fa-regular fa-user" />
+    const user = <i className="fa-regular fa-circle-user"></i>;
+    const heart = <i className="fa-regular fa-heart"></i>;
+    const cart = <i className="fa-regular fa-bag-shopping"></i>;
     return (
         <Navbar bg="light" expand="lg">
         <Container fluid>
@@ -12,25 +17,29 @@ const BootstrapNavbar = () => {
             <Navbar.Collapse id="navbarScroll">
             <Nav
                 className="mx-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
             >
-                <Nav.Link class="link" as={Link} to="/">Home</Nav.Link>
-                <Nav.Link class="link" as={Link} to="/">Shop</Nav.Link>
-                <Nav.Link class="link" as={Link} to="/">Blog</Nav.Link>
-                <Nav.Link class="link" as={Link} to="/">About us</Nav.Link>
-                <Nav.Link class="link" as={Link} to="/">Contact us</Nav.Link>
-                <Nav.Link class="link" as={Link} to="/">Dashboard</Nav.Link>
+                <Nav.Link className="link" as={Link} to="/">Home</Nav.Link>
+                <Nav.Link className="link" as={Link} to="/">Shop</Nav.Link>
+                <Nav.Link className="link" as={Link} to="/">Blog</Nav.Link>
+                <Nav.Link className="link" as={Link} to="/">About us</Nav.Link>
+                <Nav.Link className="link" as={Link} to="/">Contact us</Nav.Link>
+                <Nav.Link className="link" as={Link} to="/">Dashboard</Nav.Link>
             </Nav>
-            <Form className="d-flex">
-                <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-            </Form>
+            <Nav                
+                className="ms-auto my-2 my-lg-0 d-flex align-items-center justify-content-center"
+            >
+                <Nav.Link><b className="mx-2">{user}</b></Nav.Link>
+                <Nav.Link><b className="mx-2">{heart}</b></Nav.Link>
+                <Nav.Link><b className="mx-2">{cart}</b></Nav.Link>
+                <Form className="d-flex">
+                    <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="ms-auto"
+                    aria-label="Search"
+                    />
+                </Form>
+            </Nav>
             </Navbar.Collapse>
         </Container>
         </Navbar>
