6fe4993e25bbadc6ab6b8fd1718e037d6e4bcea5
mizan <dewan.mizanur911@gmail.com>
2022-02-27 08:45:46 +0600

    firebase authentication installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXXFHSLcsRO-oHPP2S-OLy-KmEUFs1-LE",
+    authDomain: "efurn-d1a95.firebaseapp.com",
+    projectId: "efurn-d1a95",
+    storageBucket: "efurn-d1a95.appspot.com",
+    messagingSenderId: "891132139242",
+    appId: "1:891132139242:web:e4dfb38b4bdc783a862993"
+  };
+
+export default firebaseConfig;
